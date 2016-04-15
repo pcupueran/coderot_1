46ee0067bbec3702a920fb8b7d263d037d377597
Patricia <pa1809ty@gmail.com>
2016-04-15 15:58:26 +0100

    Add status for each pull request and issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+   def status(updated_at)
+    difference = (Time.now - updated_at).seconds
+    if(difference < 1.week)
+      'less-one-week'
+    elsif( difference > 1.week && difference < 2.week)
+      'one-week'
+    elsif(difference > 2.week && difference < 3.week)
+      'two-week'
+    elsif(difference > 3.week && difference < 1.month)
+      'three-week'
+    else
+      'a-month'
+    end
+   end
 end
