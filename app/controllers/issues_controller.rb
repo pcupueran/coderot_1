930573f8cac139a2455e0178f7e44760fe26b242
Jasbir Singh <jasbirsingh279@gmail.com>
2016-04-15 14:40:46 +0100

    Paginate issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,6 @@
 class IssuesController < ApplicationController
   def index
-    @issues = github.issues.list(user: 'rails', repo: 'rails')
+    @issues = github.issues.list(user: 'rails', repo: 'rails', page: params['page'] || 1, per_page: 100)
   end
 
   private
